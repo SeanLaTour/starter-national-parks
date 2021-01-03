f0275e38b7ecdf2ab54648929e108d0526773656
Sean La Tourette <latourettes@live.com>
2021-01-02 16:11:04 -0800

    mods for practice

---
+++ index.js
@@ -0,0 +1,6 @@
+const heading = document.querySelector("h1");
+const but = document.querySelector("div .stat");
+const para = document.querySelector(".area");
+const all = document.querySelectorAll("h3");
+console.log(heading, but, para, all);
+
